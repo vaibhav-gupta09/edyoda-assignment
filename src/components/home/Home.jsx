f8990e25a5b23447b2d4f42bc67970de69bc2f3a
vaibhav-gupta09 <vaibhavgupta031209@gmail.com>
2023-04-23 01:08:12 +0530

    home analytic component done

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,24 +4,44 @@ import dataAnalysis from '../../assets/Data-Analysis.png'
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1>Upcoming Certifications</h1>
-      <div className="upcoming-certification-container">
-        <div className="analysis-container">
-          <img id="analysis-img" src={dataAnalysis} />
-          <div className="analysis-content">
-            <p id="analysis-certificate-p">Certification | Attempt 1</p>
+    <div className="home-wrapper">
+      <div className="home-container">
+        <h1>Upcoming Certifications</h1>
+        <div className="upcoming-certification-container">
+          <div className="analysis-container">
+            <img id="analysis-img" src={dataAnalysis} />
+            <div className="analysis-content">
+              <p id="analysis-certificate-p">
+                Certification &nbsp;|&nbsp; Attempt 1
+              </p>
 
-            <h3>Data Analysis Certification</h3>
+              <h3>Data Analysis Certification</h3>
 
-            <p id="analysis-status-p">
-              Completed&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;21 Mar 2022
-            </p>
+              <p id="analysis-status-p">
+                Completed&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;21 Mar 2022
+              </p>
+            </div>
+            <div className="exam-structure-container">
+              <p> Exam Structure </p>
+              <div className="round1-container">
+                <p>Round 1 &nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <p className="exam-structure-active-p">MCQs</p>
+                <p className="exam-structure-active-p"> Coding</p>
+              </div>
+              <div className="round2-container">
+                <p>Round 2&nbsp;&nbsp;&nbsp;&nbsp; </p>
+                <p className="exam-structure-active-p">Project</p>
+              </div>
+            </div>
+
+            <div className="exam-detail-btn">
+              <button>View Exam Details</button>
+            </div>
           </div>
         </div>
+        <h1>Continue Learning</h1>
+        <div className="continue-learning-container"></div>
       </div>
-      <h1>Continue Learning</h1>
-      <div className="continue-learning-container"></div>
     </div>
   );
 }
